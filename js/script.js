3a54848e2457e2df6b4857e2acd682f66e535423
nd4pa <antoine.blondeau@isen-bretagne.fr>
2019-03-05 20:19:08 +0100

    Loop works

--- js/script.js
+++ js/script.js
@@ -1,9 +1,22 @@
 function getLoop() {
-    lat = 48.4085;
-    long = -4.4984;
-    dist = 10000;
+
+    mymap.eachLayer(function (layer) {
+        mymap.removeLayer(layer);
+    });
+
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibmQ0cGEiLCJhIjoiY2l5Z2sza3o0MDAyNDJxbjQ3emd0M3BybSJ9.Ct2aqSecQmdDkHw_qrtKxA', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox.streets',
+        accessToken: 'your.mapbox.access.token'
+    }).addTo(mymap);
+
+    var distance = document.getElementById("distance").value;
+    var latitude = document.getElementById("latitude").value;
+    var longitude = document.getElementById("longitude").value;
+
     const Http = new XMLHttpRequest();
-    const url = 'http://127.0.0.1:4567/loop/' + lat + '/' + long + '/' + dist;
+    const url = 'http://127.0.0.1:4567/loop/' + latitude + '/' + longitude + '/' + distance;
     Http.open("GET", url);
     Http.send();
     Http.onreadystatechange = (e) => {
@@ -16,7 +29,7 @@ function getLoop() {
     }
 }
 
-var mymap = L.map('mapid').setView([48.4085,-4.4984], 13);
+var mymap = L.map('mapid',{ zoomControl: false}).setView([48.4085,-4.4984], 13);
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibmQ0cGEiLCJhIjoiY2l5Z2sza3o0MDAyNDJxbjQ3emd0M3BybSJ9.Ct2aqSecQmdDkHw_qrtKxA', {
                 attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -24,10 +37,15 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=p
     id: 'mapbox.streets',
     accessToken: 'your.mapbox.access.token'
 }).addTo(mymap);
+L.control.zoom({position:'topright'}).addTo(mymap);
+L.control.locate({position: 'bottomright'}).addTo(mymap);
 
 document.addEventListener('DOMContentLoaded', function () {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems);
+
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems);
 });
 
 
