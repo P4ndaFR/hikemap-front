b5e53ff4770c4b7a12a361ac0e794edefcd53cd2
dev <macbookpro@Steve.local>
2019-03-15 16:22:24 +0100

    fix map not loading

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ function getLoop() {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
         maxZoom: 18,
         id: 'mapbox.streets',
-        accessToken: 'your.mapbox.access.token';
+        accessToken: 'your.mapbox.access.token'
     }).addTo(mymap);
 
     var distance = document.getElementById("distance").value;
