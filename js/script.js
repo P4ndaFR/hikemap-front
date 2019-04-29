3bd8e9184ac98295dc6bc1f2ff21492b48aeedcc
dev <macbookpro@Steve.local>
2019-04-29 16:51:41 +0200

    Add geocoding support

--- js/script.js
+++ js/script.js
@@ -8,37 +8,31 @@ function getLoop() {
         attribution: 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a>',
         maxZoom: 18,
     }).addTo(mymap);
-    L.control.zoom({ position: 'topright' }).addTo(mymap);
-    L.control.locate({ position: 'bottomright' }).addTo(mymap);
 
     var address = document.getElementById("address").value;
+    address = address.replace(/ +(?= )/g,'+');
+    var distance = document.getElementById("distance").value;
 
+    
     const positioncall = new XMLHttpRequest();
     const positionurl = 'https://nominatim.openstreetmap.org/search?format=json&q='+address;
     positioncall.open("GET", positionurl);
     positioncall.send();
     positioncall.onreadystatechange = (e) => {
         var res = JSON.parse(positioncall.responseText);
-        console.log(res[0])
+        const loopcall = new XMLHttpRequest();
+        const loopurl = 'http://127.0.0.1:4567/loop/' + res[0].lat + '/' + res[0].lon + '/' + distance;
+        loopcall.open("GET", loopurl);
+        loopcall.send();
+        loopcall.onreadystatechange = (e) => {
+            var loopres = JSON.parse(loopcall.responseText);
+            var points = loopres.points;
+            L.marker(points[0]).addTo(mymap);
+            L.marker(points[points.length - 1]).addTo(mymap);
+            var polyline = L.polyline(points, { color: 'blue' }).addTo(mymap);
+            mymap.fitBounds(polyline.getBounds());
+        }
     }
-    /*
-    var distance = document.getElementById("distance").value;
-    var latitude = document.getElementById("latitude").value;
-    var longitude = document.getElementById("longitude").value;
-
-    const loopcall = new XMLHttpRequest();
-    const loopurl = 'http://127.0.0.1:4567/loop/' + latitude + '/' + longitude + '/' + distance;
-    loopcall.open("GET", loopurl);
-    loopcall.send();
-    loopcall.onreadystatechange = (e) => {
-        var res = JSON.parse(loopcall.responseText);
-        var points = res.points;
-        L.marker(points[0]).addTo(mymap);
-        L.marker(points[points.length - 1]).addTo(mymap);
-        var polyline = L.polyline(points, { color: 'blue' }).addTo(mymap);
-        mymap.fitBounds(polyline.getBounds());
-    }
-    */
 }
 
 var latlon = [48.4085 ,-4.5284];
