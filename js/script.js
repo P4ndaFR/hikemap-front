7e53a857cc19b5944a257b9e509d8c4b4ae028f0
nd4pa <antoine.blondeau@isen-bretagne.fr>
2019-05-24 16:13:55 +0200

    Reword GUI, added test alt routes

--- js/script.js
+++ js/script.js
@@ -19,7 +19,8 @@ function clearMap() {
 function getLoop() {
 
     clearMap();
-    var routerurl="https://hikemap-api.blondeau.me";
+    sidenav.close();
+    var routerurl="https://hikemap.blondeau.me";
     var address = document.getElementById("address").value;
     address = address.replace(/ +(?= )/g,'+');
     var distance = document.getElementById("distance").value;
@@ -27,8 +28,28 @@ function getLoop() {
     var patrimonialradio = document.getElementById('patrimonial');
     var gpsradio = document.getElementById('gps');
     var addressradio = document.getElementById('addressradio');
+    var alternative = document.getElementById('alternative');
     var vehicle = document.getElementById('vehicle').value;
     var lat,lng,stops;
+
+    var greenIcon = new L.Icon({
+        iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41]
+      });
+
+      var redIcon = new L.Icon({
+        iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41]
+      });
+      
     
     const positioncall = new XMLHttpRequest();
     const positionurl = 'https://nominatim.openstreetmap.org/search?format=json&q='+address;
@@ -55,8 +76,7 @@ function getLoop() {
                         if(loopcall.readyState == 4 && loopcall.status==200){
                             var loopres = JSON.parse(loopcall.responseText);
                             var points = loopres.points;
-                            L.marker(points[0]).addTo(mymap);
-                            //L.marker(points[points.length - 1]).addTo(mymap);
+                            L.marker(points[0], {icon: greenIcon}).bindPopup("Départ et Arrivée").addTo(mymap);
                             trace = L.polyline(points, { color: 'red' }).addTo(mymap);
                             mymap.fitBounds(trace.getBounds());
                         }
@@ -73,8 +93,7 @@ function getLoop() {
                             if(loopcall.responseText) {
                                 var loopres = JSON.parse(loopcall.responseText);
                                 var points = loopres.points;
-                                L.marker(points[0], {color: 'green' }).addTo(mymap);
-                                //L.marker(points[points.length - 1]).addTo(mymap);
+                                L.marker(points[0], {icon: greenIcon}).bindPopup("Départ et Arrivée").addTo(mymap);
                                 trace = L.polyline(points, { color: 'red' }).addTo(mymap);
                                 if(loopres.success) {
                                     for(var j=0; j < loopres.historic_points.length; j++){
@@ -96,11 +115,54 @@ function getLoop() {
                         }
                     }
                 }
+                if(alternative.checked) {
+                    
+                    const loopcall = new XMLHttpRequest();
+                    var latend = document.getElementById('alternative_end_lat').value;
+                    var lonend = document.getElementById('alternative_end_lon').value;
+                    const loopurl = routerurl+'/test/' +lat + '/' + lng + '/' + latend + '/' + lonend + '/' + vehicle;
+                    loopcall.open("GET", loopurl);
+                    loopcall.send();
+                    loopcall.onreadystatechange = (e) => { 
+                        if(loopcall.readyState == 4 && loopcall.status==200){
+                            var loopres = JSON.parse(loopcall.responseText);
+                            itineraries = loopres.itineraries;
+                            onItinerariesLoaded(itineraries);
+                        }
+                    }
+                }
             }
         }
     }
 }
+function onItinerariesLoaded(itineraries){
+    clearMap();
+    L.marker(itineraries[0].points[0]).bindPopup("Départ").addTo(mymap);
+    L.marker(itineraries[0].points[itineraries[0].points.length-1]).bindPopup("Arrivée").addTo(mymap);
+    
+    trace = L.polyline(itineraries[0].points, { color: 'red' }).addTo(mymap);
 
+    mymap.fitBounds(trace.getBounds());
+    form2 = document.getElementById('form2');
+    itinerariesdiv = document.getElementById('itinerariesdiv');
+    form2.removeChild(itinerariesdiv);
+    form2.insertAdjacentHTML('afterbegin', '<div id=itinerariesdiv></div>');
+    itinerariesdiv = document.getElementById('itinerariesdiv');
+
+    for(var j = 0 ; j < itineraries.length ; j++){
+        itinerariesdiv.insertAdjacentHTML('beforeend', '<p><label><input class="with-gap" name="group6" type="radio" id="itinerary'+j+'" value="'+j+'" onclick="showItinerary('+j+')"/><span>Itinéraire '+(j+1)+'</span></label></p>');
+    }
+}
+function showItinerary(index) {
+    clearMap();
+    L.marker(itineraries[index].points[0]).bindPopup("Départ").addTo(mymap);
+    L.marker(itineraries[index].points[itineraries[index].points.length-1]).bindPopup("Arrivée").addTo(mymap);
+    
+    trace = L.polyline(itineraries[index].points, { color: 'red' }).addTo(mymap);
+
+    modal2.close();
+    sidenav.close();
+}
 function addStopSlider(){
     div = document.getElementById('distancediv');
     div.insertAdjacentHTML('afterend', '<div id=stopsdiv> <p id="brief" >Renseignez le nombre de monument que vous voulez visiter</p><p id=stopValue>3<p><p class="range-field"><input type="range" id="stops" min="1" max="5" onchange="showStopsValue()"/></p></div>');
@@ -122,14 +184,14 @@ function toggleLocate(){
         div = document.getElementsByClassName('fixed-action-button');
         button = document.getElementsByClassName('btn-floating');
         div[0].removeChild(button[0]);
-        div[0].insertAdjacentHTML('afterbegin', '<a class="btn-floating btn-large waves-effect waves-light red" onclick="toggleLocate()"><i class="material-icons">gps_fixed</i></a>');
+        div[0].insertAdjacentHTML('afterbegin', '<a class="btn-floating btn-large waves-effect waves-light grey darken-2" onclick="toggleLocate()"><i class="material-icons">gps_fixed</i></a>');
         mustLocate = true;        
     }else{
         mustLocate = false
         div = document.getElementsByClassName('fixed-action-button');
         button = document.getElementsByClassName('btn-floating');
         div[0].removeChild(button[0]);
-        div[0].insertAdjacentHTML('afterbegin', '<a class="btn-floating btn-large waves-effect waves-light red" onclick="toggleLocate()"><i class="material-icons">gps_not_fixed</i></a>');
+        div[0].insertAdjacentHTML('afterbegin', '<a class="btn-floating btn-large waves-effect waves-light grey darken-2" onclick="toggleLocate()"><i class="material-icons">gps_not_fixed</i></a>');
         if (current_position) {
             mymap.removeLayer(current_position);
             mymap.removeLayer(current_accuracy);
@@ -156,7 +218,7 @@ function onLocationError(e) {
     div = document.getElementsByClassName('fixed-action-button');
     button = document.getElementsByClassName('btn-floating');
     div[0].removeChild(button[0]);
-    div[0].insertAdjacentHTML('afterbegin', '<a class="btn-floating btn-large waves-effect waves-light red" onclick="toggleLocate()"><i class="material-icons">gps_off</i></a>');
+    div[0].insertAdjacentHTML('afterbegin', '<a class="btn-floating btn-large waves-effect waves-light grey darken-2" onclick="toggleLocate()"><i class="material-icons">gps_off</i></a>');
     if (current_position != undefined) {
         mymap.removeLayer(current_position);
         mymap.removeLayer(current_accuracy);
@@ -182,20 +244,23 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(mymap);
 L.control.zoom({ position: 'topright' }).addTo(mymap);
 
-var div,button,mustLocate,start,stop,trace;
+var div,button,mustLocate,start,stop,trace,itineraries,modal1,modal2,sidenav;
 trace=null;
 
 mustLocate = false;
 
 document.addEventListener('DOMContentLoaded', function () {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems);
+    var tempsidenav = document.getElementById('mobile-demo');
+    sidenav = M.Sidenav.init(tempsidenav);
 
-    var elems = document.querySelectorAll('.modal');
-    var instances = M.Modal.init(elems);
+    var tempmodal1 = document.getElementById("modal1");
+   modal1 = M.Modal.init(tempmodal1);
+
+    var tempmodal2 = document.getElementById("modal2");
+    modal2 = M.Modal.init(tempmodal2);
 
     var elems = document.querySelectorAll('select');
-    var instances = M.FormSelect.init(elems, options);
+    var instances = M.FormSelect.init(elems);
 });
 
 var current_position, current_accuracy;
